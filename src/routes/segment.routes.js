1276a6c2e7337971e51a9155e6ed13d0afbe0695
wearerkdev <we.are.rkdev@gmail.com>
2024-02-14 10:01:15 -0300

    Update bruno, segment routes and docker

--- src/routes/segment.routes.js
+++ src/routes/segment.routes.js
@@ -4,8 +4,8 @@ const segmentController = require('../controllers/segment.controller');
 const segmentRouter = Router();
 
 segmentRouter.post('/novo', segmentController.addNewSegment);
+segmentRouter.get('/encontrarpor/todos/', segmentController.findAllSegments);
 segmentRouter.get('/encontrarpor/:id', segmentController.findSegmentByID);
-segmentRouter.get('/encontrartodos/', segmentController.findAllSegments);
 segmentRouter.put('/atualizar/:id', segmentController.updateSegment);
 segmentRouter.delete('/deletar/:id', segmentController.deleteSegment);
 
